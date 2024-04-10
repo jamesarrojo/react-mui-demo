f61f39360764add654a85e003dcaaee87b7ab712
jamesarrojo <jamesarrojo.01@gmail.com>
2024-04-10 18:32:07 +0800

    done with Radio Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
-import { MuiSelect } from './components/MuiSelect';
-import { MuiTextField } from './components/MuiTextField';
+import { MuiRadioButton } from './components/MuiRadioButton';
+// import { MuiSelect } from './components/MuiSelect';
+// import { MuiTextField } from './components/MuiTextField';
 // import { MuiButton } from './components/MuiButton';
 // import MuiTypography from './components/MuiTypography';
 
@@ -10,7 +11,8 @@ function App() {
             {/* <MuiTypography /> */}
             {/* <MuiButton /> */}
             {/* <MuiTextField /> */}
-            <MuiSelect />
+            {/* <MuiSelect /> */}
+            <MuiRadioButton />
         </div>
     );
 }
