0a54f65e704aff1e51cb13bdfd334cc6b45ed59d
jamesarrojo <jamesarrojo.01@gmail.com>
2024-04-13 14:47:41 +0800

    done with Chip

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
-import { MuiList } from './components/MuiList';
+import { MuiChip } from './components/MuiChip';
+// import { MuiList } from './components/MuiList';
 // import { MuiBadge } from './components/MuiBadge';
 // import { MuiAvatar } from './components/MuiAvatar';
 // import { MuiBottomNavigation } from './components/MuiBottomNavigation';
@@ -46,7 +47,8 @@ function App() {
             {/* <MuiBottomNavigation /> */}
             {/* <MuiAvatar /> */}
             {/* <MuiBadge /> */}
-            <MuiList />
+            {/* <MuiList /> */}
+            <MuiChip />
         </div>
     );
 }
